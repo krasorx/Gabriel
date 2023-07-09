d98ba3566dd2b8c7e66272bdeee2e97fd22cdb5f
krasorx <luiesp27@gmail.com>
2023-07-09 13:33:19 +0000

    seteando django con postgresql

---
+++ gabriel/gabriel/pdfChecker/admin.py
@@ -0,0 +1,8 @@
+from tokenize import group
+from unicodedata import category
+from django.contrib import admin
+from django.utils.http import urlencode
+from gabriel.pdfChecker import models
+
+
+admin.site.site_header = "PDF chequer Panel de control"
---
+++ gabriel/gabriel/pdfChecker/models.py
@@ -0,0 +1,5 @@
+import datetime
+from tabnanny import verbose
+from django.db import models
+from django.core.validators import FileExtensionValidator
+
